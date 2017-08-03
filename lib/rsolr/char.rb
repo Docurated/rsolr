f84f9336d5f0bc978831c9480ea289c707b7f5e0
Robbie Kanarek <rkanarek12@gmail.com>
2017-08-03 11:57:09 -0400

    comment out deprecation warning

--- lib/rsolr/char.rb
+++ lib/rsolr/char.rb
@@ -1,18 +1,18 @@
 # A module that contains (1) string related methods
 # @deprecated remove this module when we remove the method (duh)
 module RSolr::Char
-  
+
   # backslash everything
   # that isn't a word character
   # @deprecated - this is incorrect Solr escaping
   def escape value
-    warn "[DEPRECATION] `RSolr.escape` is deprecated (and incorrect).  Use `RSolr.solr_escape` instead."
+    # warn "[DEPRECATION] `RSolr.escape` is deprecated (and incorrect).  Use `RSolr.solr_escape` instead."
     value.gsub(/(\W)/, '\\\\\1')
   end
-  
+
   # LUCENE_CHAR_RX = /([\+\-\!\(\)\[\]\^\"\~\*\?\:\\]+)/
   # LUCENE_WORD_RX = /(OR|AND|NOT)/
-  # 
+  #
   # # More specific/lucene escape sequence
   # def lucene_escape string
   #   delim = " "
@@ -20,5 +20,5 @@ module RSolr::Char
   #     v.gsub(LUCENE_WORD_RX, '\\\\\1')
   #   }.join(delim)
   # end
-  
+
 end
